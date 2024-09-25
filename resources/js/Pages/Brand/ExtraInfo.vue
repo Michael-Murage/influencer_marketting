5eb51431039131237acff897d474d684a30c999c
Michael-Murage <mickeymurage@gmail.com>
2024-09-25 23:03:07 +0300

    profile page

--- resources/js/Pages/Brand/ExtraInfo.vue
+++ resources/js/Pages/Brand/ExtraInfo.vue
@@ -159,11 +159,11 @@ async function nextPage (e) {
 						<InputLabel>Sign Up As</InputLabel>
 						<div class="flex items-center mb-4">
 							<TextInput id="default-radio-1" type="radio" value="" name="default-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
-							<InputLabel for="default-radio-1" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Default radio</InputLabel>
+							<InputLabel for="default-radio-1" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Brand</InputLabel>
 						</div>
 						<div class="flex items-center">
 							<TextInput checked id="default-radio-2" type="radio" value="" name="default-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
-							<InputLabel for="default-radio-2" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Checked state</InputLabel>
+							<InputLabel for="default-radio-2" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Creator</InputLabel>
 						</div>
 					</div>
 					<!-- <div id="accountType" name="accountType">
--- resources/js/Pages/Profile/View.vue
+++ resources/js/Pages/Profile/View.vue
@@ -11,24 +11,45 @@ onMounted(() => {
 </script>
 <template>
   <AuthenticatedLayout>
-    <div class="py-6 px-6 w-full flex flex-col justify-center items-center">
-      <div class="">
-        <img :src="user.profile_pic[0]?.name" alt="" class="w-[200px] h-[200px] rounded-full border">
-      </div>
-      <div class="flex flex-col">
-        <div class="">
-          <p class="py-3 text-center">{{ user.name }}</p>
-          <!-- <span>+</span> -->
-          <!-- <span class="text-xl">&#9825</span> -->
-          <!-- <span>&#x2764</span> -->
+    <div class="bg-gray-100 min-h-screen p-6">
+      <header class="shadow p-4 rounded-lg mb-6">
+        <h1 class="text-3xl font-bold text-gray-800 text-center">User Profile</h1>
+      </header>
+
+      <div class="max-w-3xl mx-auto bg-white shadow-md rounded-lg overflow-hidden">
+        <div class="flex items-center p-4">
+          <img
+            :src="user.profile_pic[0]?.name"
+            alt="Profile Picture"
+            class="w-24 h-24 object-cover rounded-full border-2 border-gray-300"
+          />
+          <div class="ml-4">
+            <h2 class="text-2xl font-bold text-gray-800">{{ user.name }}</h2>
+            <p class="text-gray-600">{{ user.bio && user.bio.length > 150 ? user.bio?.slice(0, 150).concat('...') : user.bio }}</p>
+            <p class="text-gray-600 py-2">{{ user.email }}</p>
+          </div>
         </div>
-        <div>
-          <p
-            id="biography"
-            
-            class="border-gray-200 bg-transparent w-[300px]"
-            
-          >{{ user.bio }}</p>
+
+        <div class="p-4 border-t border-gray-200">
+          <h3 class="text-lg font-semibold text-gray-800">Links</h3>
+          <div class="flex mt-2">
+            <a
+              
+              :href="'https://johndoe.com'"
+              target="_blank"
+              class="text-blue-500 hover:underline mr-4"
+            >
+              Website
+            </a>
+            <a
+              
+              :href="'https://instagram.com/johndoe'"
+              target="_blank"
+              class="text-blue-500 hover:underline"
+            >
+              Instagram
+            </a>
+          </div>
         </div>
       </div>
     </div>
